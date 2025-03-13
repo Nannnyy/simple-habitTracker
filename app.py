0d5d1f102b6d2bdebdb1f938a8d0f967cde13e1f
Carol <anakfig2016@gmail.com>
2025-03-13 12:04:30 -0300

    add: displaying and saving habits, and some styling for the habitTracker

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+habits = ['Teste Hábito']
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Habit Tracker - Home")
+    return render_template("index.html", title="Habit Tracker - Home", habits = habits)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
+    if request.method == "POST":
+        habit = request.form.get("habit")
+        habits.append(habit)
+    
     return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
